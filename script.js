1ac99cc77697e8563e8c04af7c8111b2843a4e32
Nicolas Felipe da Silva <56174366+Nicolas-FelSi@users.noreply.github.com>
2023-04-11 18:53:11 -0300

    teste js

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-const numberRandom = Math.round(Math.random() * 10 )
+const numberRandom = Math.round(Math.random() * 10)
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 let tentativas = 0
 
-btnTry.addEventListener("click", (e) => {
+btnTry.addEventListener("keypress", (e) => {
     e.preventDefault()
 
     const input = document.querySelector("#inputNumber")
     tentativas++
 
-    if (input.value == numberRandom) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+    if (e.key === "Enter") {
+        if (input.value == numberRandom) {
+            screen1.classList.add("hide")
+            screen2.classList.remove("hide")
 
-        document.querySelector("#tentativaTexto").innerText = `Acertou em ${tentativas} tentativas`
+            document.querySelector("#tentativaTexto").innerText = `Acertou em ${tentativas} tentativas`
+        }
     }
-    
+
     input.value = ""
 })
-    
-btnReset.addEventListener("click", (e) => {
-    e.preventDefault()
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-    tentativas = 0
-})
 
+btnReset.addEventListener("keypress", (e) => {
+    e.preventDefault()
+    if (e.key === "Enter") {
+        screen1.classList.remove("hide")
+        screen2.classList.add("hide")
+        tentativas = 0
+    }
+})
 
 console.log(numberRandom);
