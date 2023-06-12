904b4c5b783f19268470f9e444f4783862061ad6
Nicolas felsi <nicolasfelipe3702@gmail.com>
2023-06-12 01:09:19 -0300

    to-do list iniciado

---
+++ todolist-crud/js/controller-dom.js
@@ -0,0 +1,6 @@
+export const controllerDom = {
+    inputAdd: document.getElementById("addTask"),
+    btnAdd: document.getElementById("btnAdd"),
+    textError: document.querySelector(".textError"),
+    sectionTasks: document.querySelector(".section__tasks")
+}
