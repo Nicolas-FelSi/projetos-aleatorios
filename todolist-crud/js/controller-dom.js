d7e0022e14a013179149f30b9fd20ef3420eecbf
Nicolas felsi <nicolasfelipe3702@gmail.com>
2023-06-29 19:36:36 -0300

    deletando arquivos não usados

--- todolist-crud/js/controller-dom.js
+++
@@ -1,6 +0,0 @@
-export const controllerDom = {
-    inputAdd: document.getElementById("addTask"),
-    btnAdd: document.getElementById("btnAdd"),
-    textError: document.querySelector(".textError"),
-    sectionTasks: document.querySelector(".section__tasks")
-}
